242df6567e1105e1a4783989cd353c858d38e407
Luka Tsinaridze <lukatsinaridze@gmail.com>
2022-08-31 01:45:17 +0400

    n9

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// variables
+const answerNo = document.querySelector(`.defeat`);
+const test = document.querySelector(`.test`);
+
+// rand number
+let randNumT;
+let randNumL;
+getNum = function () {
+  randNumT = Math.trunc(Math.random() * 88) + 1;
+  randNumL = Math.trunc(Math.random() * 88) + 1;
+  answerNo.style.right = `${randNumT}%`;
+  answerNo.style.top = `${randNumL}%`;
+};
+getNum();
+answerNo.addEventListener(`click`, getNum);
